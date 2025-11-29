c960c29b476e023396cd338f0ef43dec235b9a1a
Yeferson05 <yaguilar@unal.edu.co>
2025-11-29 11:59:03 -0500

    Cambios

--- homework/src/_internals/tests/test_internals.py
+++ homework/src/_internals/tests/test_internals.py
@@ -1,4 +1,38 @@
-import os
+import subprocess
+
+from homework.src._internals.read_all_lines import read_all_lines
+
+# python3 -m homework data/input data/output
+from ...wordcount import parse_args
+
+
+def test_parse_args():
+
+    result = subprocess.run(
+        ["python3", "-m", "homework", "data/input/", "data/output/"],
+        capture_output=True,
+        text=True,
+    )
+
+    assert result.returncode == 0
+    assert "input: data/input/" in result.stdout
+    assert "output: data/output/" in result.stdout
+
+
+# Función test_read_all_lines
+
+
+def test_read_all_lines():
+    input_folder = "data/input/"
+    lines = read_all_lines(input_folder)
+    assert len(lines) > 0
+    assert any(
+        "Analytics refers to the systematic computational analysis of data" in line
+        for line in lines
+    )
+
+
+"""import os
 import shutil
 
 from ..count_words import count_words
@@ -56,3 +90,4 @@ def test_write_word_counts():
 
     # Clean up
     shutil.rmtree(output_folder)
+"""
